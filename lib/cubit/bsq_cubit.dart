2f11a207e2c70549d9438d509908d08cc9edb34c
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-01-10 18:11:26 +0100

    Visually done ✅

--- lib/cubit/bsq_cubit.dart
+++ lib/cubit/bsq_cubit.dart
@@ -5,6 +5,24 @@ import 'package:flutter/services.dart';
 
 part 'bsq_state.dart';
 
+Color darken(Color color, [double amount = .1]) {
+  assert(amount >= 0 && amount <= 1);
+
+  final hsl = HSLColor.fromColor(color);
+  final hslDark = hsl.withLightness((hsl.lightness - amount).clamp(0.0, 1.0));
+
+  return hslDark.toColor();
+}
+
+Color lighten(Color color, [double amount = .1]) {
+  assert(amount >= 0 && amount <= 1);
+
+  final hsl = HSLColor.fromColor(color);
+  final hslLight = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
+
+  return hslLight.toColor();
+}
+
 class BsqCubit extends Cubit<BsqState> {
   BsqCubit() : super(const BsqInitial()) {
     if (state is BsqInitial) startBsq();
@@ -34,7 +52,7 @@ class BsqCubit extends Cubit<BsqState> {
     List<String> lines = map.split('\n');
     List<List<int>> matrix = [[]];
 
-    // TODO: Checkear que todas las lineas tengan la misma longitud
+    // Todo: Checkear que todas las lineas tengan la misma longitud
     // Converitmos el string en una matriz de 1's y 0's(obs)
     for (int i = 0; i < lines.length; i++) {
       for (int j = 0; j < lines[i].length; j++) {
@@ -44,34 +62,65 @@ class BsqCubit extends Cubit<BsqState> {
         } else if (lines[i][j] == 'o') {
           matrix[i].add(0);
         } else {
-          emit(const BsqFailed()); // TODO: put message
+          emit(const BsqFailed()); // Todo: put message
           return;
         }
       }
     }
     sendWidget(matrix);
-    // TODO: print/emit each new version, with delay
+    // Todo: print/emit each new version, with delay
     // Decompose map
     for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[i].length; i++) {
-        if (j != 0 && i != 0) {
-          if (matrix[i][j] >= 1 &&
-              (matrix[i][j] >= 1 &&
-                  matrix[i][j - 1] >= 1 &&
-                  matrix[i - 1][j] >= 1 &&
-                  matrix[i - 1][j - 1] >= 1)) {
-            matrix[i][j] = getInt(
-              matrix[i - 1][j],
-              matrix[i][j - 1],
-              matrix[i - 1][j - 1],
-            );
-          }
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (i != 0 &&
+            j != 0 &&
+            matrix[i][j] >= 1 &&
+            (matrix[i][j] >= 1 &&
+                matrix[i][j - 1] >= 1 &&
+                matrix[i - 1][j] >= 1 &&
+                matrix[i - 1][j - 1] >= 1)) {
+          matrix[i][j] = getInt(
+            matrix[i - 1][j],
+            matrix[i][j - 1],
+            matrix[i - 1][j - 1],
+          );
+          await Future.delayed(const Duration(milliseconds: 5));
+          sendWidget(matrix);
         }
       }
     }
-    sendWidget(matrix);
 
-    // TODO:  etc...ft_newtab(mapinfo, tabl);
+    // Todo:  etc...ft_newtab(mapinfo, matrix);
+
+    int maxnumx = 0;
+    int maxnumy = 0;
+    int bignum = 0;
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] > bignum) {
+          bignum = matrix[i][j];
+          maxnumx = i;
+          maxnumy = j;
+        }
+      }
+    }
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] == 0) {
+          //  print('0');
+        } else if (i <= maxnumx &&
+            j <= maxnumy &&
+            i > (maxnumx - bignum) &&
+            j > (maxnumy - bignum))
+          matrix[i][j] = -1; //   print('X');
+        else
+          //  print(' ');
+          await Future.delayed(const Duration(milliseconds: 5));
+        sendWidget(matrix);
+      }
+    }
   }
 
   void sendWidget(List<List<int>> matrix) async {
@@ -80,13 +129,18 @@ class BsqCubit extends Cubit<BsqState> {
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[i].length; j++) {
         widgets.add(
-          Text(matrix[i][j].toString()),
-          /*Container(
-            color: matrix[i][j] == 1 ? Colors.grey.shade300 : Colors.grey,
+          //Text(matrix[i][j].toString())
+
+          Container(
+            color: matrix[i][j] == 0
+                ? Colors.grey
+                : matrix[i][j] == -1
+                    ? Colors.red
+                    : darken(Colors.greenAccent, 1 / (matrix[i][j]).toDouble()),
             width: 5,
             height: 5,
             margin: const EdgeInsets.all(1),
-          ),*/
+          ),
         );
       }
     }
@@ -96,7 +150,6 @@ class BsqCubit extends Cubit<BsqState> {
       children: widgets,
     );
 
-    await Future.delayed(Duration(milliseconds: 50));
     emit(BsqLoaded(widget: gridView));
   }
 }
